04b948e92a8d9aac5d8afd613ed42fa0254b9d76
theofgt <theo.faingnaert@gmail.com>
2023-11-14 18:04:10 +0100

    test

--- config_FW1.sh
+++ config_FW1.sh
@@ -1,7 +1,55 @@
-###### Rules for FW1 ######
+#################
+# Rules for FW1 #
+#################
 
-iptables -P FORWARD DROP
-iptables -P INPUT DROP
-iptables -P OUTPUT DROP
+####### NAT rules #######
 
-iptables -A -p all -s ipsource -sport sport -d ipdestination -dport dport -j ACCEPT
+#iptables -t nat -A POSTROUTING -o eth1 -j SNAT --to 172.32.4.100 #to be continued
+
+
+####### Firewall rules #######
+
+#Incoming traffic z-mail-ssh
+
+iptables -A FORWARD -p tcp --dport 993 -d 172.31.6.5 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 25 -d 172.31.6.5 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 22 -d 172.31.6.6 -j ACCEPT
+iptables -A FORWARD -d 172.31.6.0/24 -j DROP
+
+#Outgoing traffic z-mail-ssh
+
+iptables -A FORWARD -p tcp --dport 22 -s 172.31.6.6 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 25 -s 172.31.6.5 -j ACCEPT
+iptables -A FORWARD -s 172.31.6.0/24 -j DROP
+
+#Incoming traffic z-http
+
+iptables -A FORWARD -d 172.31.5.0/24 -j DROP
+
+#Outgoing traffic z-http
+
+iptables -A FORWARD -p tcp --dport 53 -s 172.31.5.3 -j ACCEPT  
+iptables -A FORWARD -p udp --dport 53 -s 172.31.5.3 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 80 -s 172.31.5.4 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 443 -s 172.31.5.4 -j ACCEPT
+iptables -A FORWARD -s 172.31.5.0/24 -j DROP
+
+#Incoming traffic z-public
+
+iptables -A FORWARD -p tcp --dport 80 -d 172.32.5.2 -j ACCEPT 
+iptables -A FORWARD -p tcp --dport 443 -d 172.32.5.2 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 22 -s 172.31.6.6 -d 172.32.5.2 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 53 -d 172.32.5.3 -j ACCEPT
+iptables -A FORWARD -p udp --dport 53 -d 172.32.5.3 -j ACCEPT
+iptables -A FORWARD -d 172.32.5.0/24 -j DROP
+
+#Outgoing traffic z-public
+
+20. iptables -A FORWARD -p tcp --dport 53 -s 172.32.5.3 -j ACCEPT  //faire celle-ci
+21. iptables -A FORWARD -p udp --dport 53 -s 172.32.5.3 -j ACCEPT
+22. iptables -A FORWARD -s 172.32.5.0/24 -j DROP
+
+#Other
+
+iptables -A FORWARD -j LOG --log-prefix "FW1 unidentified packets"
+iptables -A FORWARD -j DROP
